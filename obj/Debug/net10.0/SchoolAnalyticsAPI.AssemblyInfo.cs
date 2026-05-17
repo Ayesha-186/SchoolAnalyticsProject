f0cb983361d96cf0de3d5dd413ddba4d5c85277f
Aiman Mansoor <aimanmnsur2@gmail.com>
2026-05-17 22:58:36 +0500

    changed html code

--- obj/Debug/net10.0/SchoolAnalyticsAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/SchoolAnalyticsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolAnalyticsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf317aa28f42f2e4ace2037f66e2a92991ef5473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bedbfe0b5b2dfa2146af51a35d925f02c7b974d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolAnalyticsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolAnalyticsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
